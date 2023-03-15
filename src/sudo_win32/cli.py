a5039abbdaa8dcb9c039cda9ab0ab4544517ede1
zackees <z@zackees.com>
2023-03-14 20:55:26 -0700

    missing files

---
+++ src/sudo_win32/cli.py
@@ -0,0 +1,18 @@
+"""
+Main entry point.
+"""
+
+import argparse
+import subprocess
+
+from .elevated_exec import elevated_exec
+
+
+def main() -> int:
+    """Main entry point for the template_python_cmd package."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd_parts", help="Command to execute as admin.", nargs="+")
+    args = parser.parse_args()
+    cmd = subprocess.list2cmdline(args.cmd_parts)
+    elevated_exec(cmd)
+    return 0
